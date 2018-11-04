b3bda759ad3f0370148c130a99ffaaba9132335a
jason zheng <101104044@nccu.edu.tw>
2018-11-05 01:05:38 +0800

    一年後，再重新修改自己寫的第一支angularjs

--- js/app.js
+++ js/app.js
@@ -7,34 +7,26 @@
     LunchCheckController.$inject = ['$scope'];
 
     function LunchCheckController($scope) {
-        $scope.result = ""; //結果文字 result text
-        $scope.TypeArea = ""; //輸入框 input
-        $scope.stringLength = 0; //輸入內容偵測 text length
-        console.log($scope.TypeArea);
+        $scope.result = "";
+        $scope.errorColor = false
         $scope.Caculate = function() {
-            var output = $scope.TypeArea.split(",").length;
-            var length = $scope.TypeArea.length;
-            var resultText = document.getElementById("result");
-            var resultTextInput = document.getElementById("lunch-menu");
-            if (length == 0) {
-                $scope.result = "Please enter data first"
-                resultText.classList.add("red");
-                resultTextInput.classList.add("red-border");
-            } else {
-                if (output > 3) {
+            $scope.TypeArea === undefined ? error() : cacalating()
+            
+            function error(){
+                $scope.result="Please enter data first"
+                $scope.errorColor=true;           
+            }
+
+            function cacalating(){
+                var typeLength=$scope.TypeArea.split(",").length>3
+                if(typeLength){
                     $scope.result = "Too Much!"
-                    resultText.classList.add("green");
-                    resultTextInput.classList.add("green-border");
-                } else {
+                    $scope.errorColor=true;
+                }else{
                     $scope.result = "Enjoy!"
-                    resultText.classList.add("green");
-                    resultTextInput.classList.add("green-border");
+                    $scope.errorColor=false;
                 }
-            };
-
-
-
-
+            }
         };
     };
 
